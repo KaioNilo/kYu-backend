4ca071c6db500b0a5dd6df0a585cac19c54a4a3e
Kaio Nilo <kaionilofreitas@gmail.com>
2026-03-03 19:33:03 -0300

    instalando coleçao Helmet e chamada no server para mais segurança

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import helmet from 'helmet';
 
 // Rotas
 import orderRoutes from './routes/orderRoutes';
@@ -12,18 +13,26 @@ import authRoutes from './routes/authRoutes';
 import { leadLimiter } from './middleware/rateLimitMiddleware';
 import { errorHandler } from './middleware/errorHandlerMiddleware';
 
-// Configuração variáveis de ambiente
+// Configuração das variáveis de ambiente
 dotenv.config();
 
 const app = express();
 
-//  Middlewares Globais
+// Middlewares Globais
+
+// Helmet
+app.use(helmet()); 
+
+// CORS
 app.use(cors());
+
+// JSON Parser
 app.use(express.json());
 
 
 // Rotas
-// Rota base para teste de saúde da API
+
+// Rota base para verificação de saúde da API
 app.get('/', (req: Request, res: Response) => {
   res.send('API K&U rodando!');
 });
@@ -32,11 +41,11 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/auth', authRoutes);
 
 // Rotas de Orçamentos
-// Limiter antes das rotas para bloquear excessos de requisição
 app.use('/api/orcamentos', leadLimiter, orderRoutes);
 
 // Rotas Admin
 app.use('/api/admin', adminRoutes);
+
 
 // Conexão BD
 const mongoURL = process.env.MONGODB_URL as string;
@@ -45,12 +54,13 @@ mongoose.connect(mongoURL)
   .then(() => console.log('✅ MongoDB conectado com sucesso!'))
   .catch((err) => console.error('❌ Erro ao conectar ao MongoDB:', err));
 
-  //Tratamento de Erros Global
+
+// Tratamento de Erros Global
 app.use(errorHandler);
 
-// Inicialização
-const PORT = Number(process.env.PORT) || 3001; 
 
+// Inicialização do Servidor
+const PORT = Number(process.env.PORT) || 3001; 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
